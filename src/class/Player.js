9a4d3da4e21429551cec2af57ebe66635243a18f
laineus <lainen000@gmail.com>
2018-06-24 18:36:34 +0900

    key control

--- src/class/Player.js
+++ src/class/Player.js
@@ -2,7 +2,7 @@ import settings from '../config/settings'
 const SPEED = 6
 export default {
   superClass: 'DisplayElement',
-  targetDeg: 0,
+  targetDeg: null,
   screenX: 0,
   screenY: 0,
   init (option) {
@@ -30,20 +30,36 @@ export default {
       this.screenX = this.x + this.field.x
       this.screenY = this.y + this.field.y
     }
-    this.tern()
+    this.keySpeed(app.keyboard)
+    this.keyTurn(app.keyboard)
+    this.tapTern()
     this.move()
+    this.fixRotation()
   },
-  tern () {
+  keySpeed (key) {
+    if (key.getKey('up')) return this.speed = SPEED * 1.5
+    if (key.getKey('down')) return this.speed = SPEED * 0.5
+    this.speed = SPEED
+  },
+  keyTurn (key) {
+    if (key.getKey('left')) this.rotation -= SPEED
+    if (key.getKey('right')) this.rotation += SPEED
+  },
+  tapTern () {
+    if (this.targetDeg === null) return
     const diff = this.targetDeg - this.rotation
+    if (Math.abs(diff) < 1) return this.targetDeg = null
     const direction = (diff > 0 && diff < 180) || diff < -180 ? 1 : -1
     const power = Math.abs(diff) < SPEED ? Math.abs(diff) : SPEED
     this.rotation += direction * power
+  },
+  fixRotation () {
     if (this.rotation > 360) this.rotation -= 360
     if (this.rotation < 0) this.rotation += 360
   },
   move () {
     const x = Math.cos(Math.degToRad(this.rotation))
     const y = Math.sin(Math.degToRad(this.rotation))
-    this.physical.force(x * SPEED, y * SPEED)
+    this.physical.force(x * this.speed, y * this.speed)
   }
 }
