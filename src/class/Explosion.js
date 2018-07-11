f7c9e13d2489fd3482b1b37b89352be0afeceade
laineus <lainen000@gmail.com>
2018-07-12 00:42:34 +0900

    explosion piece

--- src/class/Explosion.js
+++ src/class/Explosion.js
@@ -2,12 +2,13 @@ import state from '../config/state'
 import maskImage from '../utils/maskImage'
 export default {
   superClass: 'DisplayElement',
-  init (x, y) {
+  init (x, y, pieceImage = null) {
     this.superInit()
     if (Math.hypot(state.field.player.x - x, state.field.player.y - y) > 600) return
     this.addChildTo(state.field)
     this.setPosition(x, y)
     this.explosion()
+    if (pieceImage) this.piece(pieceImage)
     state.field.camera.addShock(25)
     setTimeout(() => this.remove(), 1000)
   },
@@ -27,6 +28,20 @@ export default {
         }, i ? 700 : 1500, 'easeOutCirc')
         if (i) sprite.blendMode = 'lighter'
       })
+    })
+  },
+  piece (pieceImage) {
+    Number(5).times(() => {
+      Piece(pieceImage, 3, 3)
+      .addChildTo(this)
+      .setRotation(Math.randint(0, 360))
+      .setPosition(Math.randint(-15, 15), Math.randint(-15, 15))
+      .setScale(0.2, 0.2)
+      .tweener.by({
+        x: Math.randint(-70, 70),
+        y: Math.randint(-70, 70),
+        alpha: -1
+      }, 1500, 'easeOutCirc')
     })
   }
 }
