89953e04205edfbb4ed6c626ace9f66a227b8b13
laineus <lainen000@gmail.com>
2018-08-01 23:08:18 +0900

    add start menu

--- src/class/TitleScene.js
+++ src/class/TitleScene.js
@@ -8,32 +8,53 @@ export default {
     this.superInit(option)
     this.backgroundColor = variables.color.black
     // this.bg = Sprite('title').addChildTo(this).setOrigin(0, 0).setScale(0.75, 0.75)
-    // this.logo = Sprite('logo').addChildTo(this).setScale(0.5, 0.5)
-    //                           .setPosition(this.gridX.center(), this.gridY.span(6))
-    this.onpointend = () => this.exit('Game')
-    this.label = BlurLabel({
-      text: '- CLICK TO START -',
+    this.logo = Sprite('logo').addChildTo(this).setScale(0.5, 0.5)
+                              .setPosition(this.gridX.center(), this.gridY.span(10))
+    this.addStartLabel()
+  },
+  update (app) {
+    if (this.startLabel) {
+      if (app.keyboard.getKeyDown('Z')) {
+        this.goToMenu()
+      }
+    } else if (this.list) {
+      this.cursor.update(app.keyboard)
+    }
+  },
+  addStartLabel () {
+    this.startLabel = BlurLabel({
+      text: '- PRESS \'Z\' KEY -',
       fontFamily: 'aldrich',
       fill: variables.color.white,
       fontSize: 14,
       shadowBlur: 6,
       shadowColor: variables.color.blue
-    }).addChildTo(this).setPosition(this.gridX.center(), this.gridY.span(13))
-
-    // cursor test
-    const list = labelList(['Mission', 'Customize', 'Exit'], settings.SCREEN_WIDTH_C, 460, this, { margin: 23 })
-    this.cursor = new Cursor(list, (current, other) => {
+    }).addChildTo(this).setPosition(this.gridX.center(), this.gridY.span(13.5))
+  },
+  addMenu () {
+    this.list = labelList(['Mission', 'Customize', 'Exit'], settings.SCREEN_WIDTH_C, 440, this, { margin: 23 })
+    this.cursor = new Cursor(this.list, (current, other) => {
       current.fill = variables.color.blue
       for (const row of other) {
         row.fill = variables.color.white
       }
     }, (current) => {
-      console.log(current.text)
+      switch (current.text) {
+        case 'Mission': return this.exit('Game')
+        case 'Exit': return this.backToTitle()
+      }
     }, () => {
-      console.log('end')
+      this.backToTitle()
     })
   },
-  update (app) {
-    this.cursor.update(app.keyboard)
+  goToMenu () {
+    this.startLabel.remove()
+    this.startLabel = null
+    this.addMenu()
+  },
+  backToTitle () {
+    this.list.forEach(v => v.remove())
+    this.list = null
+    this.addStartLabel()
   }
 }
