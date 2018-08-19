062ced12f725bba15b5203f08622842e011d5f45
laineus <lainen000@gmail.com>
2018-08-19 16:38:32 +0900

    turn

--- src/class/Computer.js
+++ src/class/Computer.js
@@ -39,8 +39,10 @@ export default {
   },
   ctrlTurn () {
     const degDiff = (() => {
-      if (this.x < 0 || this.x > this.field.width || this.y < 0 || this.y > this.field.height) {
-        return this.degreeDiffTo(this.field.width / 2, this.field.height / 2)
+      const xCenter = this.field.width / 2
+      const yCenter = this.field.height / 2
+      if (Math.abs(this.x - xCenter) > (xCenter - 200) || Math.abs(this.y - yCenter) > (yCenter - 200)) {
+        return this.degreeDiffTo(xCenter, yCenter)
       } else if (this.target) {
         return this.degreeDiff(this.target)
       } else if (this.subTarget) {
@@ -48,7 +50,7 @@ export default {
         const sin = Math.sin(Math.degToRad(this.subTarget.rotation + this.subTargetRotation)) * this.subTargetDistance
         return this.degreeDiffTo(this.subTarget.x + cos, this.subTarget.y + sin)
       }
-    }) ()
+    })()
     if (Math.abs(degDiff) > 1) this.turn((degDiff > 0 && degDiff < 180) || degDiff < -180 ? 1 : -1)
   },
   ctrAction () {
