0ec602deaa5cede41481e737507b19fd7a8effe5
laineus <lainen000@gmail.com>
2018-08-25 23:27:35 +0900

    scenes

--- src/config/scenes.js
+++ src/config/scenes.js
@@ -1,10 +1,20 @@
+import LoadingScene from '../class/LoadingScene'
+import TitleScene from '../class/TitleScene'
+import GameScene from '../class/GameScene'
 export default [
   {
+    className: 'LoadingScene',
+    label: 'Loading',
+    init: option => new LoadingScene(option)
+  },
+  {
     className: 'TitleScene',
-    label: 'Title'
+    label: 'Title',
+    init: option => new TitleScene(option)
   },
   {
     className: 'GameScene',
-    label: 'Game'
+    label: 'Game',
+    init: option => new GameScene(option)
   }
 ]
--- src/index.js
+++ src/index.js
@@ -3,21 +3,16 @@ import phina from 'phina.js'
 import { settings } from './config/variables'
 import assets from './config/assets'
 import scenes from './config/scenes'
-import LoadingScene from './class/LoadingScene'
-import TitleScene from './class/TitleScene'
-import GameScene from './class/GameScene'
 phina.globalize()
 
-phina.register('LoadingScene', option => new LoadingScene(option))
-phina.register('TitleScene', option => new TitleScene(option))
-phina.register('GameScene', option => new GameScene(option))
+scenes.forEach(scene => phina.register(scene.className, scene.init))
 
 phina.main(() => {
   const game = GameApp({
     title: settings.GAME_TITLE,
     width: settings.SCREEN_WIDTH,
     height: settings.SCREEN_HEIGHT,
-    startLabel: scenes[0].label,
+    startLabel: scenes[1].label,
     scenes: scenes,
     assets: assets,
     fps: settings.FPS,
