681ba870dd1c000b782d440ea12b4847dc7a60f1
laineus <lainen000@gmail.com>
2018-08-26 22:01:47 +0900

    Text

---
+++ src/class/Text.js
@@ -0,0 +1,12 @@
+import { colors } from '../config/variables'
+import BlurLabel from './BlurLabel'
+export default class Text extends BlurLabel {
+  constructor (text, size = 15) {
+    super({
+      text: text,
+      fontFamily: 'aldrich',
+      fontSize: size,
+      fill: colors.white
+    })
+  }
+}
