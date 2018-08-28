0c426fe9503693e5c7ad78927944de883aa4e704
laineus <lainen000@gmail.com>
2018-08-28 19:27:36 +0900

    PauseScene

--- src/class/PauseScene.js
+++ src/class/PauseScene.js
@@ -1,6 +1,8 @@
+import { settings } from '../config/variables'
 export default class PauseScene extends phina.display.DisplayScene {
-  constructor (option) {
-    super(option)
+  constructor (color) {
+    super({ width: settings.SCREEN_WIDTH, height: settings.SCREEN_HEIGHT })
     Object.setPrototypeOf(this, PauseScene.prototype)
+    if (color) this.backgroundColor = color
   }
 }
