d48726cd5e5302cae073d04aadce587be3a39c90
laineus <lainen000@gmail.com>
2018-09-06 00:50:23 +0900

    fix return to title

--- src/class/GameScene.js
+++ src/class/GameScene.js
@@ -14,6 +14,7 @@ export default class GameScene extends phina.display.DisplayScene {
     state.score.rescue = 0
     this.phase = 0
     this.inProgress = true
+    this.canReturn = false
     this.backgroundColor = colors.black
     // Field
     this.field = new Field().addChildTo(this)
@@ -29,24 +30,33 @@ export default class GameScene extends phina.display.DisplayScene {
     this.interface.initRadar(this.field, state.player)
   }
   update (app) {
-    state.score.time++
-    this.mission.update()
     if (!this.inProgress) {
-      if (app.keyboard.getKeyDown('Z')) this.exit('Title', { skip: 1 })
+      // TODO: move to MissionResult
+      if (this.canReturn && app.keyboard.getKeyDown('Z')) this.exit('Title', { skip: 1 })
       return
     }
     if (!state.player.isActive()) {
       this.missionFailed()
-    } else if (this.mission.functions[this.phase]()) {
+      return
+    }
+    state.score.time++
+    this.mission.update()
+    if (this.mission.functions[this.phase]()) {
       (this.phase + 1 === this.mission.functions.length) ? this.missionCompleted() : this.phase++
     }
   }
   missionCompleted () {
     new MissionResult(true, 'Time:\nKill:\nMember Death:\nRescue:\n\nRank:\nReward:', '1:14\n10\n12\n5\n\nS\n$1,000').addChildTo(this)
-    this.inProgress = false
+    this.end()
   }
   missionFailed () {
     new MissionResult(false, 'Loss:', '$1,000').addChildTo(this)
+    this.end()
+  }
+  end () {
     this.inProgress = false
+    setTimeout(() => {
+      this.canReturn = true
+    }, 3000);
   }
 }
