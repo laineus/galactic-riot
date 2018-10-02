b14f7710b9080dca889abd0607aae6b3da590b1c
laineus <lainen000@gmail.com>
2018-10-03 00:25:13 +0900

    mission

--- src/mission/mission1.js
+++ src/mission/mission1.js
@@ -1,37 +1,24 @@
 import state from '../config/state'
 import addComputer from '../utils/addComputer'
-import Gate from '../class/Gate'
 const self = {
   name: 'Mission-01',
   created: () => {
     state.field.setField('sublatant_1')
-    addComputer(200, 3200, 350, 'player', 6)
+    addComputer(2500, 2500, 350, 'player', 6)
   },
   update: () => {
   },
   functions: [
     () => {
-      addComputer(-200, 200, 165, 'enemy', 2)
-      addComputer(-200, 1600, 170, 'enemy', 3)
+      addComputer(-2500, 2500, 315, 'enemy', 2)
       return true
     },
     () => !state.field.enemy.children.length,
     () => {
-      addComputer(200, -200, 345, 'enemy', 3)
-      addComputer(200, 200, 15, 'enemy', 4)
+      addComputer(2500, -2500, 135, 'enemy', 3)
       return true
     },
-    () => !state.field.enemy.children.length,
-    () => new Gate('sublatant_2', 100, 100).setPosition(5600, 3400).setRotation(30).addChildTo(state.field.object),
-    () => state.field.is('sublatant_2'),
-    () => {
-      addComputer(-200, -200, 225, 'enemy', 5)
-      return true
-    },
-    () => !state.field.enemy.children.length,
-    () => {
-      return false
-    }
+    () => !state.field.enemy.children.length
   ]
 }
 export default self
--- src/mission/mission2.js
+++ src/mission/mission2.js
@@ -1,22 +1,34 @@
 import state from '../config/state'
 import addComputer from '../utils/addComputer'
+import Gate from '../class/Gate'
 const self = {
   name: 'Mission-02',
-  count: 0,
-  created () {
-    Player().setPosition(100, 100)
+  created: () => {
+    state.field.setField('sublatant_1')
+    addComputer(200, 3200, 350, 'player', 6)
+  },
+  update: () => {
   },
   functions: [
     () => {
-      addComputer(-200, 200, 135, 'enemy', 6)
+      addComputer(-200, 200, 165, 'enemy', 2)
+      addComputer(-200, 1600, 170, 'enemy', 3)
       return true
     },
+    () => !state.field.enemy.children.length,
     () => {
-      return state.field.enemy.children.length <= 0
+      addComputer(200, -200, 345, 'enemy', 3)
+      addComputer(200, 200, 15, 'enemy', 4)
+      return true
     },
+    () => !state.field.enemy.children.length,
+    () => new Gate('sublatant_2', 100, 100).setPosition(5600, 3400).setRotation(30).addChildTo(state.field.object),
+    () => state.field.is('sublatant_2'),
     () => {
-      return false
-    }
+      addComputer(-200, -200, 225, 'enemy', 5)
+      return true
+    },
+    () => !state.field.enemy.children.length
   ]
 }
 export default self
