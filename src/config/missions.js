e4b2434e10e917fa667749b616a8932b30385c1e
laineus <lainen000@gmail.com>
2018-10-10 00:53:43 +0900

    mission4

--- src/config/missions.js
+++ src/config/missions.js
@@ -1,12 +1,13 @@
 import mission1 from '../mission/mission1'
 import mission2 from '../mission/mission2'
 import mission3 from '../mission/mission3'
+import mission4 from '../mission/mission4'
 export default [
   { name: 'Tutorial' },
   mission1,
   mission2,
   mission3,
-  { name: 'Mission-04' },
+  mission4,
   { name: 'Mission-05' },
   { name: 'Mission-06' },
   { name: 'Mission-06' },
---
+++ src/mission/mission4.js
@@ -0,0 +1,53 @@
+import state from '../config/state'
+import addComputer from '../utils/addComputer'
+import Gate from '../class/Gate'
+const self = {
+  name: 'Mission-04',
+  created: () => {
+    state.field.setField('sublatant_3')
+    addComputer(1500, 200, 90, 'player', 6)
+  },
+  update: () => {
+  },
+  functions: [
+    () => {
+      addComputer(1600, 1100, 100, 'enemy', 3)
+      addComputer(1200, 3500, 80, 'enemy', 3)
+      return true
+    },
+    () => !state.field.enemy.children.length,
+    () => {
+      addComputer(200, 4000, 280, 'enemy', 3)
+      return true
+    },
+    () => !state.field.enemy.children.length,
+    () => {
+      addComputer(-200, -200, 260, 'enemy', 3)
+      return true
+    },
+    () => !state.field.enemy.children.length,
+    () => new Gate('sublatant_2', 100, 2800).setPosition(2600, 4700).setRotation(350).addChildTo(state.field.object),
+    () => state.field.is('sublatant_2'),
+    () => {
+      addComputer(-250, 200, 170, 'enemy', 3)
+      addComputer(-200, 850, 160, 'enemy', 3)
+      return true
+    },
+    () => !state.field.enemy.children.length,
+    () => new Gate('sublatant_1', 100, 3600).setPosition(2800, 2800).setRotation(340).addChildTo(state.field.object),
+    () => state.field.is('sublatant_1'),
+    () => {
+      addComputer(800, 200, 80, 'enemy', 4)
+      addComputer(5800, 2200, 180, 'enemy', 3)
+      return true
+    },
+    () => !state.field.enemy.children.length,
+    () => {
+      addComputer(3000, 3800, 270, 'enemy', 4)
+      addComputer(5800, 3500, 190, 'enemy', 3)
+      return true
+    },
+    () => !state.field.enemy.children.length
+  ]
+}
+export default self
