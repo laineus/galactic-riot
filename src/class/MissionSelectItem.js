7bdc690cd444e283c7d66bd4441de450a50a7ef9
laineus <lainen000@gmail.com>
2018-10-29 00:42:12 +0900

    use Text

--- src/class/MissionSelectItem.js
+++ src/class/MissionSelectItem.js
@@ -1,5 +1,6 @@
 import { colors } from '../config/variables'
 import state from '../config/state'
+import Text from './Text'
 import BlurText from './BlurText'
 export default class MissionSelectItem extends phina.display.RectangleShape {
   constructor (mission) {
@@ -16,7 +17,7 @@ export default class MissionSelectItem extends phina.display.RectangleShape {
     this.setOrigin(0, 0)
     new BlurText(mission.name, 15).addChildTo(this).setOrigin(0, 0.5).setPosition(10, 16)
     if (mission.index <= state.save.mission) {
-      new BlurText('Clear', 12, { fill: colors.yellow, shadow: colors.black }).addChildTo(this).setOrigin(1, 0.5).setPosition(390, 16)
+      new Text('Clear', 12, { fill: colors.yellow }).addChildTo(this).setOrigin(1, 0.5).setPosition(390, 16)
     }
     this.mission = mission
   }
--- src/utils/labelList.js
+++ src/utils/labelList.js
@@ -1,15 +1,7 @@
-import { colors } from '../config/variables'
-import BlurLabel from '../class/BlurLabel'
+import BlurText from '../class/BlurText'
 export default (textArray, x, y, parent, options = {}) => {
   const margin = options.margin || 20
   return textArray.map((text, i) => {
-    return new BlurLabel({
-      text: text,
-      fontFamily: 'aldrich',
-      fill: colors.white,
-      fontSize: options.fontSize || 14,
-      shadowBlur: 6,
-      shadowColor: colors.blue
-    }).addChildTo(parent).setPosition(x, y + (i * margin))
+    return new BlurText(text, options.fontSize || 14, options).addChildTo(parent).setPosition(x, y + (i * margin))
   })
 }
