e5878d1769d1ab6daec0932fea688fa583903b82
laineus <lainen000@gmail.com>
2018-11-02 23:57:44 +0900

    render each letter

--- src/class/VerticalSub.js
+++ src/class/VerticalSub.js
@@ -1,8 +1,8 @@
 import { colors } from '../config/variables'
 export default class VerticalSub extends phina.display.Label {
-  constructor (text, option) {
+  constructor (text = '', option) {
     super(Object.assign({
-      text: text ? text.split('').join('\n') : '',
+      text: text,
       fontFamily: 'aldrich',
       fontSize: 16,
       fill: colors.white,
@@ -11,10 +11,27 @@ export default class VerticalSub extends phina.display.Label {
     Object.setPrototypeOf(this, VerticalSub.prototype)
   }
   setText (text) {
-    this.text = text.split('').join('\n')
+    this.text = text
+  }
+  prerender (canvas) {
+    super.prerender(canvas)
+    const array = this._lines.map(r => r.length)
+    const maxLetter = Math.max(...array)
+    this._offsetY = (maxLetter - 1) * this.fontSize / 2
   }
   renderFill (canvas) {
     canvas.context.transform(1, -0.5, 0, 1, 0, 0)
-    super.renderFill(canvas)
+    const context = canvas.context
+    this._lines.forEach((line, lineIndex) => {
+      line.split('').forEach((letter, letterIndex) => {
+        context.fillText(letter, -lineIndex * this.lineSize - this._offset, letterIndex * this.fontSize - this._offsetY)
+      })
+    })
+  }
+  calcCanvasWidth () {
+    return super.calcCanvasHeight()
+  }
+  calcCanvasHeight () {
+    return super.calcCanvasWidth() + (this.fontSize / 2)
   }
 }
