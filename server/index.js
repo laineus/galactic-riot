654f4e5cf01c621b707ef283d87376273fed691b
laineus <lainen000@gmail.com>
2018-11-20 01:13:36 +0900

    server

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const WebSocketServer = require('websocket').server
+const http = require('http')
+const PORT = 8091
+const ALLOW_ORIGIN = 'galactic-riot.laineus.com'
+
+process.on('uncaughtException', error => console.error(`error: ${error}`))
+
+const httpServer = http.createServer((_request, response) => {
+  response.writeHead(404)
+  response.end()
+})
+httpServer.listen(PORT, () => console.log(`${new Date()} Server is listening on port ${PORT}`))
+
+webSocketServer = new WebSocketServer({ httpServer: httpServer, autoAcceptConnections: false })
+webSocketServer.on('request', request => {
+  // if (request.origin !== ALLOW_ORIGIN) {
+  //   request.reject()
+  //   return
+  // }
+  const connection = request.accept()
+  console.log(`${new Date()} Connection accepted.`)
+  connection.on('close', (reasonCode, description) => {
+    console.log(`${new Date()} Disconnected. Reson: [${reasonCode}] ${description}`)
+  })
+})
