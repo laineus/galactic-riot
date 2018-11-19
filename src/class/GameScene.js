b218a2b22ed33a59b82f6ffc2feffbae16f63abd
laineus <lainen000@gmail.com>
2018-11-19 22:55:10 +0900

    mission credit

--- src/class/GameScene.js
+++ src/class/GameScene.js
@@ -1,11 +1,12 @@
 import state from '../config/state'
-import { colors } from '../config/variables'
+import { colors, settings } from '../config/variables'
 import Field from './Field'
 import Camera from './Camera'
 import InterfaceScreen from './InterfaceScreen'
 import MissionResult from './MissionResult'
 import AddComputer from '../utils/AddComputer'
 import bgm from '../utils/bgm'
+import Text from './Text'
 export default class GameScene extends phina.display.DisplayScene {
   constructor (option) {
     super(option)
@@ -34,6 +35,8 @@ export default class GameScene extends phina.display.DisplayScene {
     this.interface.initRadar(this.field, state.player)
     // BGM
     bgm.set(this.mission.bgm)
+    // Credit
+    this.credit()
   }
   update () {
     if (!this.inProgress) return
@@ -47,6 +50,19 @@ export default class GameScene extends phina.display.DisplayScene {
     if (this.mission.functions[this.phase]()) {
       (this.phase + 1 === this.mission.functions.length) ? this.missionCompleted() : this.phase++
     }
+  }
+  credit () {
+    if (this.mission.index <= state.save.mission) return
+    const credit1 = DisplayElement().addChildTo(this).setPosition(settings.SCREEN_WIDTH - 40, settings.SCREEN_HEIGHT_C + 40)
+    new Text('Graphics & Music', 11, { align: 'right' }).addChildTo(credit1).setPosition(0, -10)
+    new Text('Laineus', 15, { align: 'right' }).addChildTo(credit1).setPosition(0, 10)
+    credit1.alpha = 0
+    credit1.tweener.wait(400).to({ alpha: 1 }, 800).wait(1600).to({ alpha: 0 }, 800).play()
+    const credit2 = DisplayElement().addChildTo(this).setPosition(settings.SCREEN_WIDTH_C, settings.SCREEN_HEIGHT_C - 10)
+    new Text('Produced by', 12).addChildTo(credit2).setPosition(0, -10)
+    new Text('Laineus', 16).addChildTo(credit2).setPosition(0, 10)
+    credit2.alpha = 0
+    credit2.tweener.wait(4700).to({ alpha: 1 }, 800).wait(1600).to({ alpha: 0 }, 800).play()
   }
   missionCompleted () {
     new MissionResult(this, true).addChildTo(this)
