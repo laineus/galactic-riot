444644da54f1224c183828f6a69a787538d35ba9
laineus <lainen000@gmail.com>
2018-11-21 00:02:41 +0900

    fix import bug

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,11 @@
-import { WebSocketServer } from 'websocket'
+import { server as WebSocketServer } from 'websocket'
 import { ALLOW_ORIGIN } from './settings'
 import httpServer from './httpServer'
 import join from './join'
 
 process.on('uncaughtException', error => console.error(`error: ${error}`))
 
-webSocketServer = new WebSocketServer({ httpServer: httpServer, autoAcceptConnections: false })
+const webSocketServer = new WebSocketServer({ httpServer: httpServer, autoAcceptConnections: false })
 webSocketServer.on('request', request => {
   if (ALLOW_ORIGIN && request.origin !== ALLOW_ORIGIN) {
     request.reject()
