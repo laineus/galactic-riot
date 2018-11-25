45a95461b761e00f3d784f40ff6e060fbe11ad22
laineus <lainen000@gmail.com>
2018-11-25 22:30:03 +0900

    push

--- server/src/push.js
+++ server/src/push.js
@@ -1,19 +1,24 @@
+require('dotenv').config()
 const webpush = require('web-push')
+const connection = require('./dbConnection').default
 
-webpush.setVapidDetails(
-  'mailto:lainen000@gmail.com',
-  '',
-  ''
-)
-
-const subscriptions = []
+webpush.setVapidDetails('mailto:lainen000@gmail.com', process.env.VAPID_PUBLIC_KEY, process.env.VAPID_PRIVATE_KEY)
 
 const params = {
-  title: 'title',
-  msg: 'test',
-  icon: 'test.png'
+  title: 'GALACTIC RIOT',
+  body: '対戦を待っているプレイヤーがいます！'
 }
 
-Promise.all(subscriptions.map(subscription => {
-  return webpush.sendNotification(subscription, JSON.stringify(params), {})
-})).then(r => console.log(r)).catch(console.error)
+connection.query('SELECT * FROM subscriptions', (error, rows) => {
+  if (error) return
+  Promise.all(rows.map(row => {
+    const subscription = {
+      endpoint: row.endpoint,
+      keys: {
+        auth: row.auth,
+        p256dh: row.p256dh
+      }
+    }
+    return webpush.sendNotification(subscription, JSON.stringify(params), {})
+  })).then(r => console.log(r)).catch(console.error)
+})
