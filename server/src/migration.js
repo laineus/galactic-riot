d95cabfa7aeee6afe186a3a545418ab066d8fe07
laineus <lainen000@gmail.com>
2018-11-25 21:29:12 +0900

    migration

---
+++ server/src/migration.js
@@ -0,0 +1,6 @@
+const connection = require('./dbConnection').default
+
+connection.connect()
+const query = 'CREATE TABLE `subscriptions` (`id` INT NOT NULL AUTO_INCREMENT, `endpoint` VARCHAR(255) NULL, `auth` VARCHAR(255) NULL, `p256dh` VARCHAR(255) NULL, PRIMARY KEY (`id`))'
+connection.query(query, (_error, results) => console.log(results))
+connection.end()
