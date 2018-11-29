0b0af55376b0596cae5e1ee7cc6b47cf86d70e3a
laineus <lainen000@gmail.com>
2018-11-29 23:27:28 +0900

    receive any

--- server/src/httpsServer.js
+++ server/src/httpsServer.js
@@ -11,10 +11,10 @@ app.use((_request, response, next) => {
   response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   next()
 })
-app.get('/', (_request, response) => {
+app.get('*', (_request, response) => {
   response.send(JSON.stringify({ rooms: rooms.map(r => r.info), publicKey: VAPID_PUBLIC_KEY }))
 })
-app.post('/', (request, response) => {
+app.post('*', (request, response) => {
   let body = ''
   request.on('data', chunk => body += chunk)
   request.on('end', () => {
